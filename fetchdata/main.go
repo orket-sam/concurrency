51c26945dde9c229cfe8750cfba61ddb245cb992
orket <developerorket@gmail.com>
2024-08-28 10:23:07 +0300

    l

--- fetchdata/main.go
+++ fetchdata/main.go
@@ -10,50 +10,47 @@ func FetchData(url string) <-chan string {
 	c := make(chan string)
 
 	go func() {
+		req, reqErr := http.NewRequest("GET", url, nil)
 
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			c <- fmt.Sprintf("Request error: %s \n", err.Error())
+		if reqErr != nil {
+			c <- fmt.Sprintf("Request error %s %s", url, reqErr.Error())
+			return
 		}
 		client := http.Client{}
-		res, responseErr := client.Do(req)
-		if responseErr != nil {
-			c <- fmt.Sprintf("Response error: %s \n", responseErr.Error())
-		} else {
-			c <- fmt.Sprintf("%s %d \n", url, res.StatusCode)
-
+		res, resError := client.Do(req)
+		if resError != nil {
+			c <- fmt.Sprintf("Response error %s %s", url, resError.Error())
+			return
 		}
-		close(c)
-
+		c <- fmt.Sprintf("%s : %s", url, res.Status)
 	}()
 
 	return c
+}
 
+func Fan(input1, input2 <-chan string) <-chan string {
+
+	c := make(chan string)
+
+	go func() {
+		c <- <-input1
+		close(c)
+	}()
+
+	go func() {
+		c <- <-input2
+
+	}()
+	return c
 }
 
 func main() {
-	urls := []string{"https://ebmsuite.com", "http://127.0.0.1/3001", "http://127.0.0.1/3002"}
-
-	inputs := []<-chan string{}
-	for _, url := range urls {
-		inputs = append(inputs, FetchData(url))
-	}
-	c := Fan(inputs)
-
-	for s := range c {
-		println(s)
-	}
-
-}
-
-func Fan(inputs []<-chan string) <-chan string {
-	c := make(chan string)
-	for _, input := range inputs {
-		go func(ch <-chan string) {
-			c <- <-ch
-		}(input)
+	input1 := FetchData("https://ebmsuite.com")
+	input2 := FetchData("http://127.0.0.1.3001")
+	c := Fan(input1, input2)
+	for i := range c {
+		println(i)
 
 	}
 
-	return c
 }
